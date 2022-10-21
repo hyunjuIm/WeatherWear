a485abcf2f6d7df1b225890295e7597f1783302b
hyunju <dear.jjwim@gmail.com>
2022-10-22 05:19:54 +0900

    [ADD] : 메인 바텀 메뉴 생성

--- app/src/main/java/com/hyunju/weatherwear/screen/main/MainActivity.kt
+++ app/src/main/java/com/hyunju/weatherwear/screen/main/MainActivity.kt
@@ -2,13 +2,57 @@ package com.hyunju.weatherwear.screen.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hyunju.weatherwear.R
+import com.hyunju.weatherwear.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    true
+                }
+                R.id.menu_weather -> {
+                    true
+                }
+                R.id.menu_wear -> {
+                    true
+                }
+                R.id.menu_setting -> {
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun showFragment(fragment: Fragment, tag: String) {
+        val findFragment = supportFragmentManager.findFragmentByTag(tag)
+        supportFragmentManager.fragments.forEach { fm ->
+            supportFragmentManager.beginTransaction().hide(fm).commitAllowingStateLoss()
+        }
+        findFragment?.let {
+            // 프래그먼트 상태 정보가 있는 경우, 보여주기만
+            supportFragmentManager.beginTransaction().show(it).commitAllowingStateLoss()
+        } ?: kotlin.run {
+            // 프래그먼트 상태 정보가 없는 경우, 추가
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, fragment, tag)
+                .commitAllowingStateLoss()
+        }
     }
 }
