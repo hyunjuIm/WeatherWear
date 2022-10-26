75f52a6d4b96eb27e209e1c1e9f3cf8761abd74f
hyunju <dear.jjwim@gmail.com>
2022-10-26 17:27:14 +0900

    [UPDATE] : CoroutineExceptionHandler를 통한 예외처리

--- app/src/main/java/com/hyunju/weatherwear/screen/base/BaseViewModel.kt
+++ app/src/main/java/com/hyunju/weatherwear/screen/base/BaseViewModel.kt
@@ -1,10 +1,16 @@
 package com.hyunju.weatherwear.screen.base
 
 import android.os.Bundle
+import androidx.annotation.StringRes
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hyunju.weatherwear.R
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.net.SocketException
+import java.net.UnknownHostException
 
 abstract class BaseViewModel : ViewModel() {
 
@@ -12,8 +18,21 @@ abstract class BaseViewModel : ViewModel() {
 
     open fun fetchData(): Job = viewModelScope.launch { }
 
+    open fun errorData(@StringRes message: Int): Job = viewModelScope.launch { }
+
     // 뷰에 대한 상태 저장, 뷰가 종료 되기 전까지는 state 유지
     open fun storeState(stateBundle: Bundle) {
         this.stateBundle = stateBundle
+    }
+
+    protected val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        throwable.printStackTrace()
+
+        when (throwable) {
+            is SocketException -> errorData(R.string.fail_get_information)
+            is HttpException -> errorData(R.string.fail_get_information)
+            is UnknownHostException -> errorData(R.string.fail_get_information)
+            else -> errorData(R.string.request_error)
+        }
     }
 }
--- app/src/main/java/com/hyunju/weatherwear/screen/main/home/HomeViewModel.kt
+++ app/src/main/java/com/hyunju/weatherwear/screen/main/home/HomeViewModel.kt
@@ -3,17 +3,15 @@ package com.hyunju.weatherwear.screen.main.home
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.hyunju.weatherwear.R
-import com.hyunju.weatherwear.data.entity.WeatherEntity
 import com.hyunju.weatherwear.data.repository.map.MapRepository
 import com.hyunju.weatherwear.data.repository.weather.WeatherRepository
-import com.hyunju.weatherwear.data.response.wether.CategoryType
-import com.hyunju.weatherwear.data.response.wether.Items
 import com.hyunju.weatherwear.screen.base.BaseViewModel
-import com.hyunju.weatherwear.util.date.getCurrentTime
 import com.hyunju.weatherwear.util.date.getTodayDate
 import com.hyunju.weatherwear.util.location.TO_GRID
 import com.hyunju.weatherwear.util.location.convertGridGPS
+import com.hyunju.weatherwear.util.weather.parseWeatherData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,60 +23,41 @@ class HomeViewModel @Inject constructor(
 
     val homeStateLiveData = MutableLiveData<HomeState>(HomeState.Uninitialized)
 
-    fun updateLocationWeather(latitude: Double, longitude: Double) = viewModelScope.launch {
-        homeStateLiveData.value = HomeState.Loading
+    // 위치로 날씨 정보 받아오기
+    fun updateLocationWeather(latitude: Double, longitude: Double) =
+        viewModelScope.launch(exceptionHandler) {
+            homeStateLiveData.value = HomeState.Loading
 
-        val addressInfo = mapRepository.getReverseGeoInformation(latitude, longitude)
-        val location = addressInfo?.toSearchInfoEntity(latitude, longitude) ?: run {
-            homeStateLiveData.value = HomeState.Error(R.string.can_not_load_address_info)
-            return@launch
-        }
-
-        val grid = convertGridGPS(TO_GRID, latitude, longitude)
-        val weatherInfo = weatherRepository.getWeather(
-            dataType = "JSON",
-            numOfRows = 250,
-            pageNo = 1,
-            baseDate = getTodayDate(),
-            baseTime = "0200",
-            nx = grid.x.toInt(),
-            ny = grid.y.toInt()
-        ) ?: run {
-            homeStateLiveData.value = HomeState.Error(R.string.can_not_load_weather_info)
-            return@launch
-        }
-
-        val weatherEntity = parseWeatherData(weatherInfo)
-
-        homeStateLiveData.value = HomeState.Success(
-            location = location,
-            weatherInfo = weatherEntity
-        )
-    }
-
-    private fun parseWeatherData(weatherInfo: Items): WeatherEntity {
-        val weatherEntity = WeatherEntity()
-
-        weatherInfo.item?.forEach {
-            weatherEntity.run {
-                if (it?.category == CategoryType.TMN) TMN = fcstValueToInt(it.fcstValue)
-                if (it?.category == CategoryType.TMX) TMX = fcstValueToInt(it.fcstValue)
-
-                if (it?.fcstTime == getCurrentTime()) {
-                    date = it.baseDate ?: ""
-                    time = it.fcstTime
-                    if (it.category == CategoryType.POP) POP = fcstValueToInt(it.fcstValue)
-                    if (it.category == CategoryType.PTY) PTY = fcstValueToInt(it.fcstValue)
-                    if (it.category == CategoryType.REH) REH = fcstValueToInt(it.fcstValue)
-                    if (it.category == CategoryType.SKY) SKY = fcstValueToInt(it.fcstValue)
-                    if (it.category == CategoryType.TMP) TMP = fcstValueToInt(it.fcstValue)
-                    if (it.category == CategoryType.WSD) WSD = fcstValueToDouble(it.fcstValue)
-                    this.x = it.nx ?: -1
-                    this.y = it.ny ?: -1
-                }
+            val addressInfo = mapRepository.getReverseGeoInformation(latitude, longitude)
+            val location = addressInfo?.toSearchInfoEntity(latitude, longitude) ?: run {
+                homeStateLiveData.value = HomeState.Error(R.string.can_not_load_address_info)
+                return@launch
             }
-        }
-        return weatherEntity
-    }
 
+            val grid = convertGridGPS(TO_GRID, latitude, longitude)
+            val weatherInfo = weatherRepository.getWeather(
+                dataType = "JSON",
+                numOfRows = 250,
+                pageNo = 1,
+                baseDate = getTodayDate(),
+                baseTime = "0200",
+                nx = grid.x.toInt(),
+                ny = grid.y.toInt()
+            ) ?: run {
+                homeStateLiveData.value = HomeState.Error(R.string.can_not_load_weather_info)
+                return@launch
+            }
+
+            val weatherEntity = parseWeatherData(weatherInfo)
+
+            homeStateLiveData.value = HomeState.Success(
+                location = location,
+                weatherInfo = weatherEntity
+            )
+        }
+
+    override fun errorData(message: Int): Job = viewModelScope.launch {
+        homeStateLiveData.value = HomeState.Loading
+        homeStateLiveData.value = HomeState.Error(message)
+    }
 }
